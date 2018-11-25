b51f6ac3922b252680280d295eaa7a0714d55750
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-25 16:42:30 +0100

    Kreiran konstruktor

--- src/ba/unsa/etf/rpr/tutorijal06/MjestoRodjenja.java
+++ src/ba/unsa/etf/rpr/tutorijal06/MjestoRodjenja.java
@@ -5,6 +5,10 @@ import javafx.beans.property.SimpleStringProperty;
 public class MjestoRodjenja {
     private SimpleStringProperty mjestoRodjenja;
 
+    public MjestoRodjenja(SimpleStringProperty mjestoRodjenja) {
+        this.mjestoRodjenja = mjestoRodjenja;
+    }
+
     public String getMjestoRodjenja() {
         return mjestoRodjenja.get();
     }
