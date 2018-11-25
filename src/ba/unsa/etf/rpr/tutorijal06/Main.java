bb54412ea6d450e556beab45026eb0e8b6f51531
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-25 16:09:37 +0100

    Napravljen osnovni izgled formulara

--- src/ba/unsa/etf/rpr/tutorijal06/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Main.java
@@ -6,13 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("upis.fxml"));
+        primaryStage.setTitle("Upis");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
     }
 
