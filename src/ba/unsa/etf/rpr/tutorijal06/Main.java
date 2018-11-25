34d90333c48e0a1e499b2e4eb223b888b53668d7
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-25 16:24:15 +0100

    Napravljen osnovni izgled formulara

--- src/ba/unsa/etf/rpr/tutorijal06/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
@@ -18,8 +20,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
