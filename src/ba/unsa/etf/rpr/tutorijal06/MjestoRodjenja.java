aa3bdd56ae41487ef085ed0cd4305bcc450df7aa
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-25 16:40:11 +0100

    Kreirani setteri i getteri

--- src/ba/unsa/etf/rpr/tutorijal06/MjestoRodjenja.java
+++ src/ba/unsa/etf/rpr/tutorijal06/MjestoRodjenja.java
@@ -3,6 +3,17 @@ package ba.unsa.etf.rpr.tutorijal06;
 import javafx.beans.property.SimpleStringProperty;
 
 public class MjestoRodjenja {
-    private SimpleStringProperty mjesto;
+    private SimpleStringProperty mjestoRodjenja;
 
+    public String getMjestoRodjenja() {
+        return mjestoRodjenja.get();
+    }
+
+    public void setMjestoRodjenja(String mjestoRodjenja) {
+        this.mjestoRodjenja.set(mjestoRodjenja);
+    }
+
+    public SimpleStringProperty mjestoRodjenjaProperty() {
+        return mjestoRodjenja;
+    }
 }
