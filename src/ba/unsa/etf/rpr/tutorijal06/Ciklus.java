d956d5ec5ff50b1840e74434ecace4cd0c731b19
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-26 16:17:33 +0100

    Kreirana klasa Ciklus

---
+++ src/ba/unsa/etf/rpr/tutorijal06/Ciklus.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.tutorijal06;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Ciklus {
+    private SimpleStringProperty ciklus = new SimpleStringProperty("");
+
+    public Ciklus(String ciklus) {
+        this.ciklus = new SimpleStringProperty (ciklus);
+    }
+
+    public Ciklus() {
+    }
+
+    public String getCiklus() {
+        return ciklus.get();
+    }
+
+    public void setCiklus(String ciklus) {
+        this.ciklus.set(ciklus);
+    }
+
+    public SimpleStringProperty ciklusProperty() {
+        return ciklus;
+    }
+
+    @Override
+    public String toString() {
+        return ciklus.get();
+    }
+}
