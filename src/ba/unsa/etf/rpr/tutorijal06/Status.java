df3979874c4f4234e6a805d3291d0e5b43f0aff1
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-26 16:29:03 +0100

    Kreirana klasa Status

---
+++ src/ba/unsa/etf/rpr/tutorijal06/Status.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.tutorijal06;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Status {
+    private SimpleStringProperty status = new SimpleStringProperty("");
+
+    public Status(String status) {
+        this.status = new SimpleStringProperty (status);
+    }
+
+    public Status() {
+    }
+
+    public String getStatus() {
+        return status.get();
+    }
+
+    public void setStatus(String status) {
+        this.status.set(status);
+    }
+
+    public SimpleStringProperty statusProperty() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return status.get();
+    }
+}
