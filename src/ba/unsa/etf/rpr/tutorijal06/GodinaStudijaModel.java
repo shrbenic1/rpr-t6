60f14f59183e86790bd5190c085dae0db56f1609
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-26 16:05:51 +0100

    Kreirana klasa GodinaStudijaModel

---
+++ src/ba/unsa/etf/rpr/tutorijal06/GodinaStudijaModel.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr.tutorijal06;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class GodinaStudijaModel {
+    private ObservableList<GodinaStudija> godineStudija = FXCollections.observableArrayList();
+    private ObjectProperty<GodinaStudija> trenutnaGodina = new SimpleObjectProperty<>();
+
+    public void setTrenutnaGodina(GodinaStudija trenutnaGodina) {
+        this.trenutnaGodina.set(trenutnaGodina);
+    }
+
+    public ObservableList<GodinaStudija> getGodineStudija() {
+        return godineStudija;
+    }
+
+    public GodinaStudija getTrenutnaGodina() {
+        return trenutnaGodina.get();
+    }
+
+    public ObjectProperty<GodinaStudija> trenutnaGodinaProperty() {
+        return trenutnaGodina;
+    }
+
+    public void dodajGodine() {
+        godineStudija.add(new GodinaStudija("AE"));
+        godineStudija.add(new GodinaStudija"RI"));
+        godineStudija.add(new GodinaStudija("EE"));
+        godineStudija.add(new GodinaStudija("TK"));
+    }
+}
