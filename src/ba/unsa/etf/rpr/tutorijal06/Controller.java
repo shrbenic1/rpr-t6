18905a35991deb2ee88bca86ade9f9de4545e2be
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-11-27 02:11:15 +0100

    Reformatiran kod

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -45,11 +45,11 @@ public class Controller {
     }
 
     private boolean validnoImePrezime(String n) {
-        if(n.length() < 1 || n.length() > 20) {
+        if (n.length() < 1 || n.length() > 20) {
             return false;
         }
-        for(int i = 0; i < n.length(); i++){
-            if(n.charAt(i) < 'A' || n.charAt(i) > 'Ž' && n.charAt(i) < 'a' || n.charAt(i) > 'ž') {
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) < 'A' || n.charAt(i) > 'Ž' && n.charAt(i) < 'a' || n.charAt(i) > 'ž') {
                 return false;
             }
         }
@@ -57,8 +57,8 @@ public class Controller {
     }
 
     private boolean validanIndeks(String n) {
-        for(int i = 0; i < n.length(); i++) {
-            if(n.charAt(i) < '0' || n.charAt(i) > '9') {
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) < '0' || n.charAt(i) > '9') {
                 return false;
             }
         }
@@ -66,25 +66,25 @@ public class Controller {
     }
 
     private boolean validanJmbg(String n) {
-        if(n.length() != 13) {
+        if (n.length() != 13) {
             return false;
         }
-        for(int i = 0; i < n.length(); i++) {
-            if(n.charAt(i) < '0' || n.charAt(i) > '9') {
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) < '0' || n.charAt(i) > '9') {
                 return false;
             }
         }
         //int L = 11 - (( 7*(getNumericValue(n.charAt(0))+getNumericValue(n.charAt(6))) + 6*(getNumericValue(n.charAt(1))+getNumericValue(n.charAt(7))) + 5*(getNumericValue(n.charAt(2))+getNumericValue(n.charAt(8))) + 4*(getNumericValue(n.charAt(3))+getNumericValue(n.charAt(9))) + 3*(getNumericValue(n.charAt(4))+getNumericValue(n.charAt(10))) + 2*(getNumericValue(n.charAt(5))+getNumericValue(n.charAt(11))) ) % 11);
-        int L = 11 - ((7 * (n.charAt(0) - '0' + n.charAt(6) - '0') + 6 * (n.charAt(1) - '0' + n.charAt(7) - '0') + 5 * (n.charAt(2) - '0' + n.charAt(8) - '0') + 4 * (n.charAt(3) - '0' + n.charAt(9) - '0') + 3 * (n.charAt(4) - '0' + n.charAt(10) - '0') + 2 * (n.charAt(5) - '0' + n.charAt(11) - '0'))%11);
-        if(L>9) {
+        int L = 11 - ((7 * (n.charAt(0) - '0' + n.charAt(6) - '0') + 6 * (n.charAt(1) - '0' + n.charAt(7) - '0') + 5 * (n.charAt(2) - '0' + n.charAt(8) - '0') + 4 * (n.charAt(3) - '0' + n.charAt(9) - '0') + 3 * (n.charAt(4) - '0' + n.charAt(10) - '0') + 2 * (n.charAt(5) - '0' + n.charAt(11) - '0')) % 11);
+        if (L > 9) {
             L = 0;
         }
         return L == (n.charAt(12) - '0');
     }
 
-    private  boolean validanKontaktTelefon(String n) {
-        for(int i = 0; i < n.length(); i++) {
-            if(n.charAt(i) < '0' || n.charAt(i) > '9') {
+    private boolean validanKontaktTelefon(String n) {
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) < '0' || n.charAt(i) > '9') {
                 return false;
             }
         }
@@ -488,10 +488,9 @@ public class Controller {
     }
 
 
-
     public void date(ActionEvent actionEvent) {
         LocalDate datum1 = datum.getValue();
-        if(validnostDatuma(datum1)) {
+        if (validnostDatuma(datum1)) {
             datum.getStyleClass().removeAll("poljeNijeIspravno");
             datum.getStyleClass().add("poljeIspravno");
         } else {
@@ -502,24 +501,24 @@ public class Controller {
 
     private boolean validnostDatuma(LocalDate datum1) {
         Date date = new Date();
-        if(jmbg.getCharacters().toString().isEmpty()) {
+        if (jmbg.getCharacters().toString().isEmpty()) {
             return false;
         }
-        if(date.getYear() < datum1.getYear() && date.getMonth() < datum1.getMonth().getValue() && date.getDay() < datum1.getDayOfYear()) {
+        if (date.getYear() < datum1.getYear() && date.getMonth() < datum1.getMonth().getValue() && date.getDay() < datum1.getDayOfYear()) {
             return false;
         } else {
-            if(Integer.parseInt(jmbg.getCharacters().toString().substring(4, 7)) < 900) {
-                if(datum1.getYear() != Integer.parseInt("2" + jmbg.getCharacters().toString().substring(4, 7))) {
+            if (Integer.parseInt(jmbg.getCharacters().toString().substring(4, 7)) < 900) {
+                if (datum1.getYear() != Integer.parseInt("2" + jmbg.getCharacters().toString().substring(4, 7))) {
                     return false;
                 }
             } else {
-                if(datum1.getYear() != Integer.parseInt("1"+ jmbg.getCharacters().toString().substring(4, 7))) {
+                if (datum1.getYear() != Integer.parseInt("1" + jmbg.getCharacters().toString().substring(4, 7))) {
                     return false;
                 }
             }
-            if(datum1.getMonth().getValue() != Integer.parseInt(jmbg.getCharacters().toString().substring(2, 4).replace("0", ""))) {
+            if (datum1.getMonth().getValue() != Integer.parseInt(jmbg.getCharacters().toString().substring(2, 4).replace("0", ""))) {
                 return false;
-            } else if(datum1.getDayOfYear() != Integer.parseInt(jmbg.getCharacters().toString().substring(0, 2).replace("0", ""))) {
+            } else if (datum1.getDayOfYear() != Integer.parseInt(jmbg.getCharacters().toString().substring(0, 2).replace("0", ""))) {
                 return false;
             } else {
                 return true;
@@ -529,7 +528,7 @@ public class Controller {
 
     public void potvrdi(ActionEvent actionEvent) {
         EmailValidator validator = EmailValidator.getInstance();
-        if(validnoImePrezime(ime.getCharacters().toString()) && validnoImePrezime(prezime.getCharacters().toString()) && validanIndeks(indeks.getCharacters().toString()) && validanJmbg(jmbg.getCharacters().toString()) && validnostDatuma(datum.getValue()) && validanKontaktTelefon(kontaktTelefon.getCharacters().toString()) && validator.isValid(email.getText()) && !(izborMjestaRodjenja.getPromptText().isEmpty()) && !(izborStatusa.getValue() == null) && !(izborCiklusa.getValue() == null) && !(izborGodineStudija.getValue() == null) && !(izborOdsjeka.getValue() == null)) {
+        if (validnoImePrezime(ime.getCharacters().toString()) && validnoImePrezime(prezime.getCharacters().toString()) && validanIndeks(indeks.getCharacters().toString()) && validanJmbg(jmbg.getCharacters().toString()) && validnostDatuma(datum.getValue()) && validanKontaktTelefon(kontaktTelefon.getCharacters().toString()) && validator.isValid(email.getText()) && !(izborMjestaRodjenja.getPromptText().isEmpty()) && !(izborStatusa.getValue() == null) && !(izborCiklusa.getValue() == null) && !(izborGodineStudija.getValue() == null) && !(izborOdsjeka.getValue() == null)) {
             potvrda.getStyleClass().removeAll("poljeNijeIspravno");
             potvrda.getStyleClass().add("poljeIspravno");
             System.out.println("Ime: " + ime.getCharacters().toString());
@@ -538,10 +537,10 @@ public class Controller {
             System.out.println("JBMG: " + jmbg.getCharacters().toString());
             //System.out.println("Datum rođenja: " + datum.getValue());
             System.out.println("Mjesto rođenja: " + izborMjestaRodjenja.getPromptText());
-            if(!kontaktAdresa.getCharacters().toString().isEmpty()) {
+            if (!kontaktAdresa.getCharacters().toString().isEmpty()) {
                 System.out.println("Kontakt adresa: " + kontaktAdresa.getCharacters().toString());
             }
-            if(!kontaktTelefon.getCharacters().toString().isEmpty()) {
+            if (!kontaktTelefon.getCharacters().toString().isEmpty()) {
                 System.out.println("Kontakt telefon: " + kontaktTelefon.getCharacters().toString());
             }
             System.out.println("E-mail adresa: " + email.getCharacters().toString());
@@ -549,7 +548,7 @@ public class Controller {
             System.out.println("Godina studija: " + izborGodineStudija.getValue());
             System.out.println("Ciklus studija: " + izborCiklusa.getValue());
             System.out.println("Status: " + izborStatusa.getValue());
-            if(boracka.isSelected()) {
+            if (boracka.isSelected()) {
                 System.out.println("Pripada boračkim kategorijama");
             } else {
                 System.out.println("Ne pripada boračkim kategorijama");
